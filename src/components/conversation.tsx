bd7295a8e006d6b7bc8ceeddd682a6eb86e53ea4
wcyat <sleep@wcyat.me>
2022-01-08 22:48:45 +0800

    update

--- src/components/conversation.tsx
+++ src/components/conversation.tsx
@@ -5,25 +5,24 @@ import Title from "./title";
 import axios from "axios";
 import {isMobile} from 'react-device-detect';
 import ResponsiveAppbar from "./Appbar";
+import DOMPurify from 'dompurify';
+import parse from 'html-react-parser';
 export default class Conversation extends React.Component<any> {
     constructor(props:any) {
         super(props);
         this.getdata = this.getdata.bind(this);
-        this.build = this.build.bind(this);
-    }
+        this.build = this.build.bind(this);}
     o : JSX.Element[] = [];
     conversation:any = {};
     users:any = {};
     state = {
-        ready : false
-    }
+        ready : false}
     async getdata() {
         await axios.get(`/api/thread/${this.props.id}/conversation`).then(res => {
             this.conversation = res.data;})
         await axios.get(`/api/thread/${this.props.id}/users`).then(res => {
             this.users = res.data;})
-        this.setState({ready : true});
-    }
+        this.setState({ready : true});}
     build() {
         Object.entries(this.conversation.conversation).map((entry:any) => {
             this.o.push(
@@ -31,10 +30,8 @@ export default class Conversation extends React.Component<any> {
                         id={entry[0]} 
                         op={this.users[entry[1].user].name === this.conversation.op ? true : false} 
                         sex={this.users[entry[1].user].sex === "male" ? true : false}>
-                            {entry[1].comment}</Comment>
-            )
-        })
-    }
+                            {parse(DOMPurify.sanitize(entry[1].comment))}</Comment>
+            )})}
     render() {
         if (!this.state.ready) {this.getdata(); return <p style={{color: "white"}}>Please wait...</p>};
         this.build();
@@ -46,6 +43,4 @@ export default class Conversation extends React.Component<any> {
                 {this.o}
               </Box>
           </div>
-        )
-    }
-}
+        )}}
