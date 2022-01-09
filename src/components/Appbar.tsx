e14be5adb60072fa3aac0982723f7fbed4be0f2e
wcyat <sleep@wcyat.me>
2022-01-09 12:49:02 +0800

    Fix Appbar on mobile

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -25,17 +25,6 @@ const ResponsiveAppBar = () => {
               onClick={handleOpenNavMenu} color="inherit">
               <MenuIcon />
             </IconButton>
-            <Menu id="menu-appbar" anchorEl={anchorElNav}
-              anchorOrigin={{vertical: 'bottom', horizontal: 'left',}}
-              keepMounted transformOrigin={{vertical: 'top',
-                horizontal: 'left',}}
-              open={Boolean(anchorElNav)} onClose={handleCloseNavMenu}
-              sx={{display: { xs: 'block', md: 'none' }}}>
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>))}
-            </Menu>
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <Menu id="menu-appbar" anchorEl={anchorElNav}
