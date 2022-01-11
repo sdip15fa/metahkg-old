a709678145834dba2012f73f2273b11d9929a2f0
wcyat <sleep@wcyat.me>
2022-01-11 21:10:09 +0800

    update

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box, List, ListItem, Drawer, Divider, ListItemIcon, ListItemText, Button } from '@mui/material';
+import { Mail as MailIcon, MoveToInbox as InboxIcon, Menu as MenuIcon } from '@mui/icons-material';
+export default class SideBar extends React.Component<any> {
+    constructor(props:any) {
+        super(props);
+        this.state.open = this.props.open || false;
+        this.toggleDrawer = this.toggleDrawer.bind(this);}
+    state = {
+        open : false}
+    toggleDrawer =
+        (open: boolean) =>
+        (event: React.KeyboardEvent | React.MouseEvent) => {
+          if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' ||
+              (event as React.KeyboardEvent).key === 'Shift')) {return;}
+          this.setState({open : open});};
+    links = {
+        1 : ['/signin', '/register', '/create'],
+        2 : ['/about', 'https://gitlab.com/wcyat-me/metahkg']}
+    render () {
+        return (
+            <div>
+            <div>
+              <Button sx={{margin: '10px', height: '40px', width:'40px'}} variant="text" onClick={this.toggleDrawer(true)}><MenuIcon style={{color : 'white'}}/></Button>
+            </div>
+            <Drawer anchor='left' open={this.state.open} onClose={this.toggleDrawer(false)}>
+            <Box sx={{width: 250}} role="presentation" onClick={this.toggleDrawer(false)} onKeyDown={this.toggleDrawer(false)}>
+               <List>
+        {['Sign in', 'Register', 'Create Topic'].map((text, index) => (
+          <a href={this.links[1][index]}><ListItem button key={text}>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary={text}/>
+          </ListItem></a>))}
+      </List>
+      <Divider/>
+      <List>
+        {['About', 'Source code'].map((text, index) => (
+          <a href={this.links[2][index]}><ListItem button key={text}>
+            <ListItemIcon>
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem></a>))}
+      </List>
+    </Box>
+    </Drawer>
+    </div>)}}
