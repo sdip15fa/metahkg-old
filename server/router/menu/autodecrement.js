362046fd3d919da96e43c80e8c1648ca1f2c32e6
wcyat <sleep@wcyat.me>
2022-01-18 23:06:54 +0800

    voting api

--- server/router/menu/autodecrement.js
+++ server/router/menu/autodecrement.js
@@ -2,8 +2,10 @@ const { MongoClient } = require("mongodb");
 const { mongouri } = require("../../common");
 async function autodecrement() {
     const client = new MongoClient(mongouri);
+    try{
     await client.connect();
     const hottest = client.db("metahkg-threads").collection("hottest");
-    await hottest.updateMany({}, {$inc: {c : -1}});
+    await hottest.updateMany({}, {$inc: {c : -1}});}
+    finally{ await client.close()}
 }
 module.exports = { autodecrement }
